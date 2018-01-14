5e031e4d6339d57a6bee93b73b83f4dadd6af64d
Treasure <treasurewmj@foxmail.com>
2018-01-14 22:35:28 +0800

    尝试本地缓存依旧失败

--- src/middleware/koa-mongo.ts
+++ src/middleware/koa-mongo.ts
@@ -1,8 +1,10 @@
 import { getMongo } from '../db/dbMongo';
 
+let mongo = Object.create(null);
+
 // 这里不能return一个函数，避免每次都创建一个mongodb连接
 export async function koaMongo (ctx, next) {
-  let mongo = await getMongo();
+    mongo = await getMongo();
     ctx.mongo = mongo;
     await next();
 }
