59987b04a23fca9eb380ba82261994c729238159
Treasure <treasurewmj@foxmail.com>
2018-01-16 23:59:13 +0800

    测试JOI

--- test.js
+++ test.js
@@ -1,7 +1,46 @@
-let datalist = [], idList = [];
+const Joi = require('joi');
 
-let selectArr = [];
-for(let id of idList) {
-  let temp = datalist.find(item => item.id = id);
-  selectArr.push(temp);
+const schema = {
+  name: Joi.string().min(3).max(10),
+  password: Joi.string().min(5).max(20).required(),
+  age: Joi.number().min(0).max(100)
 }
+
+const test = {
+  name: '1',
+  password: '2',
+  age: 22
+}
+
+const test2 = {
+  name: 'Treasure',
+  password: 'Sunshine',
+  age: 22
+}
+
+const test3 = {
+  password: 'Sunshine',
+  age: 22
+}
+
+let result = Joi.validate(test, schema);
+let result2 = Joi.validate(test2, schema);
+let result3 =Joi.validate(test3, schema);
+
+// result.then(res => {
+//   console.log('success', res)
+// }).catch(err => {
+//   console.warn('error', err);
+// })
+
+// result2.then(res => {
+//   console.log('success', res)
+// }).catch(err => {
+//   console.log('error', err)
+// })
+
+result3.then(res => {
+  console.log('success', res);
+}).catch(err => {
+  console.warn('error', err);
+})
