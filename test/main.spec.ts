44add3beb21a6447652c4118cf815f0bfbe0eb11
Treasure <treasurewmj@foxmail.com>
2018-02-01 22:49:42 +0800

    新增覆盖区域 以及mongodb测试

--- test/main.spec.ts
+++ test/main.spec.ts
@@ -1,14 +1,48 @@
 import server from '../src/main';
 const request = require('supertest');
 
-afterAll(() => {
-  server.close();
-});
+import mongoDb from '../src/db/mongoDb';
 
-describe('server API', () => {
-    test('Get Info', async () => {
-      let res = await request(server).get('/resume/info');
-      console.log(res.body);
-      expect(res.body).not.toBeNull();
-    })
-});
+// afterAll(() => {
+//   server.close();
+// });
+
+// describe('server API', () => {
+//     test('Get Info', async () => {
+//       let res = await request(server).get('/resume/info');
+//       console.log(res.body);
+//       expect(res.body).not.toBeNull();
+//     })
+// });
+
+afterAll(() => {
+  return mongoDb.getCollenction('user').then(collection => collection.drop());
+})
+
+describe.only('Mongodb', () => {
+  test('find', async () => {
+    let res = await mongoDb.findOne('user', { username: 'Treasure' });
+    console.log(res);
+    expect(res).toBeNull();
+  });
+
+  test('insert many', async () => {
+    let people = [
+      {
+        name: '徐菡'
+      },
+      {
+        name: '张宇翔'
+      },
+      {
+        name: '牟月章'
+      },
+      {
+        name: '董浩然'
+      }
+    ];
+    let res = await mongoDb.insertMany('user', people);
+    console.log(res);
+    expect(res.insertedCount).toBe(4);
+  })
+})
