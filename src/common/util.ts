c8e6268685f2e6642bea5baa345b7301927f37d3
Treasure <treasurewmj@foxmail.com>
2018-02-05 21:09:40 +0800

    公私钥加密解密

--- src/common/util.ts
+++ src/common/util.ts
@@ -1,6 +1,6 @@
 import * as crypto from "crypto";
-import config from '../config';
 import { v4 } from 'uuid';
+import { Buffer } from "buffer";
 
 /**
  * sha1加密
@@ -8,7 +8,7 @@ import { v4 } from 'uuid';
  * @param {string?} key 加密的key 默认在config
  */
 function sha1(str: string, key?:string): string {
-  let sha1 = crypto.createHmac('sha1', key || config.keyword);
+  let sha1 = crypto.createHmac('sha1', key);
   sha1.update(str);
   return sha1.digest('hex');
 }
@@ -19,7 +19,7 @@ function sha1(str: string, key?:string): string {
  * @param {string?} key 加密的key 默认在config
  */
 function md5(str: string, key?:string): string {
-  let sha1 = crypto.createHmac('sha1', key || config.keyword);
+  let sha1 = crypto.createHmac('sha1', key);
   sha1.update(str);
   return sha1.digest('hex');
 }
@@ -31,7 +31,30 @@ function uuid(): string {
   return v4();
 }
 
+/**
+ * 公钥加密
+ * @param {string} plainText 待加密的密文
+ * @param {string} pubkey 公钥
+ * @returns {Buffer} 加密后的Buffer
+ */
+function encrypt(plainText: string, pubkey: string): Buffer {
+  return crypto.publicEncrypt(pubkey, Buffer.from(plainText));
+}
+
+/**
+ * 
+ * @param {Buffer} encrypted 待解密的密文
+ * @param {string} privateKey 私钥
+ * @returns 明文
+ */
+function decrypt(encrypted: Buffer, privateKey: string): string {
+  return crypto.privateDecrypt(privateKey, encrypted).toString();
+}
+
 export default {
   sha1,
-  md5
+  md5,
+  uuid,
+  encrypt,
+  decrypt
 }
